05d467917fd0bb636342ab7cf5eac54cbc438eab
Alberto Zarraga <zarraga555@gmail.com>
2020-01-28 02:17:53 -0400

    archivos modificados

---
+++ view/inferior.php
@@ -0,0 +1,16 @@
+
+
+      <!-- Footer -->
+      <footer class="sticky-footer bg-white">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span>Copyright &copy; Lepetit 2020</span>
+          </div>
+        </div>
+      </footer>
+    </div>
+  </div>
+
+</body>
+
+</html>
---
+++ view/superior.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Lepetit - Dashboard</title>
+  <link rel="icon" href="img/favicon-96x96.png" type="image/png">
+  <link href="css/style.css" rel="stylesheet">
+  <script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
+
+</head>
+
+<body>
+
+
+
+  <div class="wrapper">
+    <div class="slid">
+    <img src="img/favicon-96x96.png" alt="">
+        <ul class="sidebar"> 
+            
+            <li><a href="#"><i class="fas fa-home"></i>Dashboard</a></li>
+            <li><a href="#"><i class="fas fa-user"></i>Usuarios</a></li>
+            <li><a href="#"><i class="fas fa-address-card"></i>Empleados</a></li>
+            <li><a href="#"><i class="fas fa-boxes"></i>Almacen</a></li>
+            <li><a href="#"><i class="fas fa-parachute-box"></i>Proveedores</a></li>
+        </ul> 
+    </div>
+    <div class="main_content">
+        <nav class="header">Welcome!! Have a nice day.</nav>  
+        <div class="info">
+   
+      </div>
+    
+
